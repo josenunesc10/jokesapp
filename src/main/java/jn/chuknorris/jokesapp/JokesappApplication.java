937c586196f932c9f22263f7d693db16c310a87a
josenunesc10 <jose.nunes.c10@sapo.pt>
2022-05-10 14:23:14 +0200

    Explored Spring configuration alternatives

--- src/main/java/jn/chuknorris/jokesapp/JokesappApplication.java
+++ src/main/java/jn/chuknorris/jokesapp/JokesappApplication.java
@@ -2,8 +2,10 @@ package jn.chuknorris.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokesappApplication {
 
     public static void main(String[] args) {
